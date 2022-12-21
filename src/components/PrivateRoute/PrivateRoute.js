dcd62c5bc319e61dc3f0c876664643825cb34636
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-12-22 01:05:57 +0600

    Project-Update

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -4,7 +4,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../UserContext/UserContext';
 
 const PrivateRoute = ({ children }) => {
-    const { user, loading } = useContext(AuthContext)
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
     if (loading) {
         return <div className="text-center">
